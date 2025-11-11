9856644711c4a8704d6a0617d858d151867714f9
AakrshitThakur <thakurraakrshitt@gmail.com>
2025-11-11 21:40:05 +0530

    UploadFile component ui enhanced

--- src/components/customs/sections/UploadFile.tsx
+++ src/components/customs/sections/UploadFile.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import Image from "next/image";
 import File from "./File";
 import Progress from "@/components/customs/Progress";
 import { useSelector } from "react-redux";
@@ -13,8 +14,14 @@ export default function UploadFile() {
       <form>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-1 p-5 sm:p-6 md:p-7">
           <div className="flex flex-col justify-center items-center gap-1 p-3 sm:p-5 md:p-7">
-            <div className="flex flex-col justify-center items-center gap-1 border rounded-md p-3 sm:p-4 md:p-5">
-              <FaUserCircle className="w-[5rem] sm:w-[6rem] md:w-[7rem] h-auto" />
+            <div className="flex flex-col justify-center items-center gap-1 rounded-md p-3 sm:p-4 md:p-5">
+              <Image
+                src="/cv.png"
+                alt="CV image"
+                width={100}
+                height={100}
+                className="w-[5rem] sm:w-[6rem] md:w-[7rem] h-auto"
+              />
               <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-center leading-none">ResumeReviewer</h3>
               {loading && <Progress />}
             </div>
